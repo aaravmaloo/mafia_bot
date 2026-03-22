cbf23ddb148301dbd3290cb93113515d5c1df801
Aarav Maloo <aaravmaloo06@gmail.com>
2026-03-22 11:37:09 +0530

    create player structs

---
+++ models/player.go
@@ -0,0 +1,12 @@
+package models
+
+import "go.mau.fi/whatsmeow/types"
+
+type Player struct {
+	JID             types.JID
+	Name            string
+	Role            Role
+	Alive           bool
+	NightActionDone bool
+	Phone           string
+}
